1d8493b8ab4da57dcb1d0a5dc28ed0dec97e0246
Youheng Luo <luoyouheng@YouhengdeMacBook-Pro.local>
2020-04-14 16:24:23 +0800

    完善

---
+++ models/statistical_method/11.crf/crf.py
@@ -0,0 +1,25 @@
+# -*- encoding:utf-8 -*-
+# @Time    : 2020/4/13 6:14 下午
+# @Author  : HengGeZhiZou <1018676477@qq.com>
+from models.model import Model
+
+
+class CRF(Model):
+    def __init__(self):
+        super().__init__()
+
+    def train(self, **args):
+        pass
+
+    def predict(self, **args):
+        pass
+
+    def dump(self, **args):
+        pass
+
+    def load(self, **args):
+        pass
+
+
+if __name__ == '__main__':
+    pass
